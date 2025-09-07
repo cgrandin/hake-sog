3b1bf149fa547a928eb24dc3aa6dfd89c9bef102
Chris Grandin <chris.grandin@gmail.com>
2025-09-07 08:39:56 -0700

    Add retrospective bash scripts

---
+++ bash-scripts/create-rds-retrospectives.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# Create an RDS file for each retrospective model found in the
+# `$model_path/retrospectives` directory. The `retrospectives` part
+# of the path is appended  in the `create_rds_files_retro()` function
+
+# The path structure is as follows
+# /srv/hake/models/2023/01-version/01-base-models/01-base/
+#  ^   ^    ^      ^    ^          ^              ^
+#  |   |    |      |    |          |              |
+#  |   |    |      |    |          |              $model_name
+#  |   |    |      |    |          $type_path
+#  |   |    |      |    $version_path
+#  |   |    |      $year
+#  |   |    $models_path
+#  \  /
+#   ||
+#   $project_path
+
+# Create the variable $assess_year containing the current year unless it
+# is currently December, in which case it will be the current year + 1
+# Enter a year as an argument here to force it to be that year, even if
+# December.
+. ./get-assess-year.sh
+
+repo_path="/home/grandin/github/pacific-hake/hake-sog"
+# If running on a local machine and the model folder is in your
+# repo root, uncomment the next line and comment the line after it
+#project  path=`Rscript -e "cat(dirname(here::here()))"`
+project_path="/srv/hake-sog"
+
+models_path="models"
+# *Never* change `year_path` manually here. Instead, to assign a manual year,
+# add it as an argument to the `get-assess-year.sh` call above
+year_path=$assess_year
+version_path="01-version"
+type_path="01-base-models"
+model="01-base"
+
+model_path=$project_path/$models_path/$year_path/$version_path/$type_path/$model
+[[ ! -d $model_path ]] && { echo "Error: Directory $model_path does not \
+exist, bailing out." ; exit 1; }
+
+(trap 'kill 0' SIGINT; \
+  printf "\nCreating RDS files for base model retrospectives\n"; \
+  Rscript -e " \
+  setwd('$repo_path'); \
+  suppressPackageStartupMessages(devtools::load_all()); \
+  create_rds_files_retro(model_path = '$model_path', \
+                         verbose = TRUE, \
+                         overwrite = TRUE)"; \
+  printf "\nFinished creating RDS file for base model retrospectives\n"; \
+)
---
+++ bash-scripts/run-retrospectives.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# To run retrospectives -1 year through -5 years:
+# ./run-retrospectives.sh 1 2 3 4 5
+#
+# The path structure is as follows
+# /srv/hake/models/2023/01-version/01-base-models/01-base/
+#  ^   ^    ^      ^    ^          ^              ^
+#  |   |    |      |    |          |              |
+#  |   |    |      |    |          |              $model_name
+#  |   |    |      |    |          $type_path
+#  |   |    |      |    $version_path
+#  |   |    |      $year
+#  |   |    $models_path
+#  \  /
+#   ||
+#   $project_path
+
+repo_path="/home/grandin/github/pacific-hake/hake-sog"
+# Create the variable $assess_year containing the current year unless it
+# is currently December, in which case it will be the current year + 1
+# Enter a year as an argument here to force it to be that year, even if
+# December.
+. ./get-assess-year.sh
+
+models_path="/srv/hake-sog/models"
+# *Never* change `year_path` manually - See `get-assess-year.sh` call above
+year_path=$assess_year
+version_path="01-version"
+type_path="01-base-models"
+model_name="01-base"
+
+ss_exe="ss3"
+num_chains=8
+num_samples=2000
+num_warmup_samples=250
+adapt_delta=0.8
+run_extra_mcmc=TRUE
+
+model_path=$models_path/$year_path/$version_path/$type_path/$model_name
+[[ ! -d $model_path ]] && { echo "Error: Directory $model_path does not \
+exist, bailing out." ; exit 1; }
+
+for arg; do
+  (trap 'kill 0' SIGINT; \
+  echo; \
+  Rscript -e " \
+  setwd('$repo_path'); \
+  devtools::load_all(); \
+  run_retrospectives(model_path = '$model_path', \
+                     retro_yrs = $arg, \
+                     num_chains = $num_chains, \
+                     num_samples = $num_samples, \
+                     num_warmup_samples = $num_warmup_samples, \
+                     adapt_delta = $adapt_delta, \
+                     run_extra_mcmc = $run_extra_mcmc, \
+                     fn_exe = '$ss_exe')" \
+  #> /dev/null 2>&1; \
+  printf "\nRetrospective -$arg completed\n" \
+  ) &
+done
